c35f4d1685976f4f64e0d57632787494fde9ca08
ErniePearce <erniepearce2003@gmail.com>
2025-04-08 12:47:59 +0100

    home build

---
+++ Home build/CMakeFiles/launch-Computer_Graphics_Coursework-RelWithDebInfo.cmd
@@ -0,0 +1,9 @@
+cd C:/Users/ernie/Documents/GitHub/Computer-Graphics-Coursework/source/
+set 
+
+if [%1]==[--debugger] (
+	SHIFT
+)
+
+"C:\Users\ernie\Documents\GitHub\Computer-Graphics-Coursework\Home build\RelWithDebInfo\Computer_Graphics_Coursework.exe"  
+pause
